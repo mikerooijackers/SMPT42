a33be34643d0fea6f647bb86b78ed9f53ced8a8b
Niek Kruse <niekkruse70@gmail.com>
2014-12-11 16:26:50 +0100

    PlanActivity partial class

---
+++ Backend/HomePlanBackend/HomePlan.Entities/Extended/PlanActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomePlan.Shared.DTO;
+
+// ReSharper disable once CheckNamespace
+namespace HomePlan.Entities
+{
+    public partial class PlanActivity
+    {
+        public PlanActivityDto ToPlanActivityDto()
+        {
+            return new PlanActivityDto()
+            {
+                EndTimeTicks = this.EndTime.Ticks,
+                StartTimeTicks = this.StartTime.Ticks,
+                PlanActivityId = this.PlanActivityID,
+                Type = this.Type
+            };
+        }
+    }
+}
