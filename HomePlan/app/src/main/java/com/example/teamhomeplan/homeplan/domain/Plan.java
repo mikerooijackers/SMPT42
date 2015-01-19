b05b4b494ef4702c0a10162e5ebc4d7d5a356915
Niek Kruse <niekkruse70@gmail.com>
2015-01-19 14:21:01 -0800

    Plan domain class

--- HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/domain/Plan.java
+++ HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/domain/Plan.java
@@ -1,13 +1,72 @@
 package com.example.teamhomeplan.homeplan.domain;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Niek on 19/01/15.
- *
+ * <p/>
  * Domain class for a plan received from the backend.
  */
 public class Plan {
 
-    public String planID;
 
-    
+    @SerializedName("PlanId")
+    private String planID;
+
+    @SerializedName("StartDateTimeMillis")
+    private double startDateTimeMillis;
+
+    @SerializedName("EndDateTimeMillis")
+    private double endDateTimeMillis;
+
+    @SerializedName("PlannedActivities")
+    private List<PlanActivity> plannedActivities;
+
+    @SerializedName("ActualActivities")
+    private List<PlanActivity> actualActivities;
+
+    public Plan() {
+    }
+
+    public String getPlanID() {
+        return planID;
+    }
+
+    public void setPlanID(String planID) {
+        this.planID = planID;
+    }
+
+    public double getStartDateTimeMillis() {
+        return startDateTimeMillis;
+    }
+
+    public void setStartDateTimeMillis(double startDateTimeMillis) {
+        this.startDateTimeMillis = startDateTimeMillis;
+    }
+
+    public double getEndDateTimeMillis() {
+        return endDateTimeMillis;
+    }
+
+    public void setEndDateTimeMillis(double endDateTimeMillis) {
+        this.endDateTimeMillis = endDateTimeMillis;
+    }
+
+    public List<PlanActivity> getPlannedActivities() {
+        return plannedActivities;
+    }
+
+    public void setPlannedActivities(List<PlanActivity> plannedActivities) {
+        this.plannedActivities = plannedActivities;
+    }
+
+    public List<PlanActivity> getActualActivities() {
+        return actualActivities;
+    }
+
+    public void setActualActivities(List<PlanActivity> actualActivities) {
+        this.actualActivities = actualActivities;
+    }
 }
