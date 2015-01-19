e8348d8b64a314a785982e909b28f645ec58bbde
Edwin Lambregts <edwin.lambregts0@gmail.com>
2015-01-19 18:02:06 +0100

    AdjustDaylength

--- HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/AdjustDayLengthActivity.java
+++ HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/AdjustDayLengthActivity.java
@@ -3,14 +3,18 @@ package com.example.teamhomeplan.homeplan;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TimePicker;
-
+import android.widget.Toast;
+import com.example.teamhomeplan.homeplan.callback.AdjustDayLengthCallback;
+import com.example.teamhomeplan.homeplan.exception.ServiceException;
+import com.example.teamhomeplan.homeplan.tasks.AdjustDayLengthTask;
 import java.util.UUID;
 
 /**
  * Created by Edwin on 17-1-2015.
  */
-public class AdjustDayLengthActivity extends Activity {
+public class AdjustDayLengthActivity extends Activity implements AdjustDayLengthCallback {
 
     private UUID userId;
     private Context context = this;
@@ -24,5 +28,37 @@ public class AdjustDayLengthActivity extends Activity {
         timePicker.setIs24HourView(true);
         timePicker.setCurrentHour(8);
         timePicker.setCurrentMinute(0);
+
+        findViewById(R.id.btndaylengthSave).setOnClickListener(btnClick);
+    }
+
+    protected View.OnClickListener btnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int hour = ((TimePicker) findViewById(R.id.tpAdjustDayLength)).getCurrentHour();
+            int min = ((TimePicker) findViewById(R.id.tpAdjustDayLength)).getCurrentMinute();
+
+            long length;
+
+            if (hour == 0 && min == 0) {
+                Toast.makeText(context, "Vul een langere duratie in", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                length = hour + min;
+            }
+
+            AdjustDayLengthTask adjustDayLengthTask = new AdjustDayLengthTask(AdjustDayLengthActivity.this, length);
+            adjustDayLengthTask.execute();
+        }
+    };
+
+    @Override
+    public void afterAdjustSuccess() {
+        this.finish();
+    }
+
+    @Override
+    public void afterAdjustFail(ServiceException exception) {
+        Toast.makeText(context, "Er ging iets fout met het verwerken", Toast.LENGTH_SHORT).show();
     }
 }
