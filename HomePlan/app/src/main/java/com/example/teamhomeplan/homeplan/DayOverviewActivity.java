244ee0b783d362e57d0608c627a0c49206556000
Niek Kruse <niekkruse70@gmail.com>
2015-01-19 23:23:17 -0800

    Draw a pie chart after generating workday plan

--- HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/DayOverviewActivity.java
+++ HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/DayOverviewActivity.java
@@ -1,20 +1,32 @@
 package com.example.teamhomeplan.homeplan;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.teamhomeplan.homeplan.domain.Plan;
+import com.example.teamhomeplan.homeplan.domain.PlanActivity;
 import com.example.teamhomeplan.homeplan.helper.Session;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.interfaces.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Legend;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by Niek on 19-1-2015
- *
+ * <p/>
  * Activity for creating the charts and keeping track of the current day of work.
  */
-public class DayOverviewActivity extends ActionBarActivity {
+public class DayOverviewActivity extends Activity implements OnChartValueSelectedListener {
+
+    private Plan loadedPlan;
+    private PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +34,94 @@ public class DayOverviewActivity extends ActionBarActivity {
         setContentView(R.layout.activity_day_overview);
 
         //The current loaded and generated plan.
-        Plan plan = Session.currentLoadedPlan;
+        this.loadedPlan = Session.currentLoadedPlan;
+
+        this.pieChart = (PieChart) findViewById(R.id.day_overview_chart);
+
+        pieChart.setHoleColor(getResources().getColor(android.R.color.black));
+        pieChart.setHoleRadius(60f);
+        pieChart.setCenterText("TODAY: TIME REMAINING");
+        pieChart.setDrawYValues(false);
+        pieChart.setDrawCenterText(true);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setRotationAngle(0);
+        pieChart.setDrawXValues(true);
+
+        pieChart.setUsePercentValues(true);
+
+
+        //TODO: SET DATA HERE.
+        setChartData();
+
+        pieChart.animateXY(1500, 1500);
+
+        Legend l = pieChart.getLegend();
+        l.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+
 
         //TODO: draw the pie chart from here.
     }
 
 
+    private void setChartData() {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        ArrayList<Entry> yVals = new ArrayList<>();
+
+        //Total amount of milliseconds for the plan
+        double totalMillis = loadedPlan.getEndDateTimeMillis() - loadedPlan.getStartDateTimeMillis();
+
+        for (int i = 0; i < loadedPlan.getPlannedActivities().size(); i++) {
+            PlanActivity pa = loadedPlan.getPlannedActivities().get(i);
+            String message;
+            switch (pa.getType()) {
+                case PERSONAL:
+                    message = pa.getUserActivity().getName();
+                    break;
+                case INCIDENTAL:
+                    message = "Incident";
+                    break;
+                default:
+                    message = "Work";
+                    break;
+            }
+
+            xVals.add(message);
+
+            //Duration of this PlanActivity
+            double durationMillis = pa.getEndTimeMillis() - pa.getStartTimeMillis();
+
+            yVals.add(new Entry((float) (durationMillis / totalMillis) * 100, i, pa));
+        }
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        PieDataSet dataSet = new PieDataSet(yVals, "Day planning");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(xVals, dataSet);
+        pieChart.setData(data);
+
+
+        pieChart.highlightValues(null);
+
+        pieChart.invalidate();
+    }
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_day_overview, menu);
-        return true;
+    public void onValueSelected(Entry entry, int i) {
+        //TODO: show more info about the selected item
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onNothingSelected() {
+        //TODO: hide the extra info we got from selecting part of the pie chart.
     }
 }
--- HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/domain/PlanActivity.java
+++ HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/domain/PlanActivity.java
@@ -25,4 +25,43 @@ public class PlanActivity {
     @SerializedName("UserActivity")
     private UserActivity userActivity;
 
+    public String getPlanActivityId() {
+        return planActivityId;
+    }
+
+    public void setPlanActivityId(String planActivityId) {
+        this.planActivityId = planActivityId;
+    }
+
+    public PlanActivityType getType() {
+        return type;
+    }
+
+    public void setType(PlanActivityType type) {
+        this.type = type;
+    }
+
+    public long getStartTimeMillis() {
+        return startTimeMillis;
+    }
+
+    public void setStartTimeMillis(long startTimeMillis) {
+        this.startTimeMillis = startTimeMillis;
+    }
+
+    public long getEndTimeMillis() {
+        return endTimeMillis;
+    }
+
+    public void setEndTimeMillis(long endTimeMillis) {
+        this.endTimeMillis = endTimeMillis;
+    }
+
+    public UserActivity getUserActivity() {
+        return userActivity;
+    }
+
+    public void setUserActivity(UserActivity userActivity) {
+        this.userActivity = userActivity;
+    }
 }
