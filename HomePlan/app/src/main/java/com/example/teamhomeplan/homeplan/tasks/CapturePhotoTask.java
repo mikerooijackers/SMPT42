6d6eabf23762b1ac992748427e947aa352d370f9
Edwin Lambregts <edwin.lambregts0@gmail.com>
2015-01-20 14:12:09 +0100

    Implementation CapturePhotoTask

--- HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/tasks/CapturePhotoTask.java
+++ HomePlan/app/src/main/java/com/example/teamhomeplan/homeplan/tasks/CapturePhotoTask.java
@@ -1,11 +1,23 @@
 package com.example.teamhomeplan.homeplan.tasks;
 
 import android.os.AsyncTask;
-
 import com.example.teamhomeplan.homeplan.callback.CapturePhotoCallback;
 import com.example.teamhomeplan.homeplan.exception.AsyncTaskException;
+import com.example.teamhomeplan.homeplan.exception.JsonException;
 import com.example.teamhomeplan.homeplan.exception.ServiceException;
 import com.example.teamhomeplan.homeplan.helper.Constants;
+import com.example.teamhomeplan.homeplan.helper.GsonFactory;
+import com.example.teamhomeplan.homeplan.helper.Session;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by Edwin on 19-Jan-15.
@@ -28,7 +40,31 @@ public class CapturePhotoTask extends AsyncTask<Void, Void, String> {
         String image = "";
 
         try {
-            // TODO fill in thissssssssssssssssssssssss
+            HttpClient client = new DefaultHttpClient();
+            Gson gson = GsonFactory.createGson();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.add("userId", gson.toJsonTree(Session.authenticatedUser.getUserId()));
+            jsonObject.add("photo", gson.toJsonTree(this.encodedImage));
+
+            HttpPost post = new HttpPost(url);
+            String s = jsonObject.toString();
+            StringEntity se = new StringEntity(s, "UTF-8");
+            se.setContentType("application/json");
+            post.setEntity(se);
+
+            HttpResponse response = client.execute(post);
+            StatusLine statusLine = response.getStatusLine();
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            response.getEntity().writeTo(out);
+
+            String respj = out.toString();
+
+            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                image = gson.fromJson(respj, String.class);
+            } else {
+                this.serviceException = gson.fromJson(respj, JsonException.class);
+            }
         } catch (Exception ex) {
             this.serviceException = new AsyncTaskException(ex);
         }
